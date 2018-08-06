7bd92113d41625c94631f3462e777e328769f398
sankhumpha <sankhumpha84@gmail.com>
2018-08-06 08:37:10 +0700

    remove config

--- application/config/database.php
+++ application/config/database.php
@@ -95,32 +95,4 @@ $db['default'] = array(
 	'save_queries' => TRUE
 );
 
-/*
 
-Database:	poszeeco_edu
-Host:	localhost
-Username:	poszeeco_edu
-Password:	U4GqDtk7
-*/
-
-$db['host'] = array(
-	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'smxcente',
-	'password' => '044126251x@',
-	'database' => 'smxcente_main',
-	'dbdriver' => 'mysqli',
-	'dbprefix' => '',
-	'pconnect' => FALSE,
-	'db_debug' => (ENVIRONMENT !== 'production'),
-	'cache_on' => FALSE,
-	'cachedir' => '',
-	'char_set' => 'utf8',
-	'dbcollat' => 'utf8_general_ci',
-	'swap_pre' => '',
-	'encrypt' => FALSE,
-	'compress' => FALSE,
-	'stricton' => FALSE,
-	'failover' => array(),
-	'save_queries' => TRUE
-);
